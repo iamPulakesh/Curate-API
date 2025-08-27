b5100b2546c4144f1e34d50b1532aa6102989ca6
Pulakesh Mondal <pulak4467@gmail.com>
2025-08-28 00:12:24 +0530

    updated user_routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app import models,schemas
+from app import models,schemas,crud
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -18,3 +18,17 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @router.get("/")
 def get_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
+
+# delete user
+@router.delete("/delete={user_id}", response_model=schemas.UserResponse)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.delete_user(db, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+# get users by name
+@router.get("/name={name}", response_model=list[schemas.UserResponse])
+def get_users_by_name(name: str, db: Session = Depends(get_db)):
+    users = db.query(models.User).filter(models.User.name.ilike(f"%{name}%")).all()
+    return users
