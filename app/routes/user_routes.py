f9cc0f5c691c62c82e71304de62bc63d21bf7dec
Pulakesh Mondal <pulak4467@gmail.com>
2025-08-28 01:12:25 +0530

    update_user_root

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -32,3 +32,11 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 def get_users_by_name(name: str, db: Session = Depends(get_db)):
     users = db.query(models.User).filter(models.User.name.ilike(f"%{name}%")).all()
     return users
+
+# update user
+@router.put("/update={user_id}", response_model=schemas.UserResponse)
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.update_user(db, user_id, user)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
