569b5c668a821e8cebe0990e76f0abe2cd0b298f
Pulakesh Mondal <pulak4467@gmail.com>
2025-08-28 00:12:06 +0530

    updated items_routes

--- app/routes/item_routes.py
+++ app/routes/item_routes.py
@@ -1,21 +1,29 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import crud, schemas
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
-#post items
+# create items
 @router.post("/", response_model=schemas.ItemResponse)
 def create_item(item: schemas.ItemCreate, owner_id: int, db: Session = Depends(get_db)):
     return crud.create_item(db=db, item=item, owner_id=owner_id)
 
-#get items
+# get items
 @router.get("/", response_model=list[schemas.ItemResponse])
 def list_items(db: Session = Depends(get_db)):
     return crud.get_items(db)
 
-#get items by genre
+# get items by genre
 @router.get("/genre={genre}", response_model=list[schemas.ItemResponse])
 def list_items_by_genre(genre: str, db: Session = Depends(get_db)):
     return crud.get_items_by_genre(db, genre)
+
+# delete item
+@router.delete("/delete={item_id}", response_model=schemas.ItemResponse)
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.delete_item(db, item_id)
+    if not db_item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
