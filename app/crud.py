9f6ce016d366a20247ccead3c0a5a1115fb900be
Pulakesh Mondal <pulak4467@gmail.com>
2025-08-28 00:08:51 +0530

    updated crud operations

--- app/crud.py
+++ app/crud.py
@@ -12,6 +12,13 @@ def create_user(db: Session, user: schemas.UserCreate):
 def get_users(db: Session):
     return db.query(models.User).all()
 
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
+
 # item CRUD
 def create_item(db: Session, item: schemas.ItemCreate, owner_id: int = None):
     db_item = models.Item(**item.dict(), owner_id=owner_id)
@@ -25,3 +32,10 @@ def get_items(db: Session):
 
 def get_items_by_genre(db: Session, genre: str):
     return db.query(models.Item).filter(models.Item.genre.ilike(f"%{genre}%")).all()
+
+def delete_item(db: Session, item_id: int):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+    return db_item
