4d4596558ba7003adb58d764026fa9ef28909457
Pulakesh Mondal <pulak4467@gmail.com>
2025-08-29 20:39:20 +0530

    updated CRUD

--- app/crud.py
+++ app/crud.py
@@ -49,3 +49,13 @@ def delete_item(db: Session, item_id: int):
         db.delete(db_item)
         db.commit()
     return db_item
+
+def update_item(db: Session, item_id: int, item: schemas.ItemUpdate):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not db_item:
+        return None
+    for key, value in item.model_dump(exclude_unset=True).items():
+        setattr(db_item, key, value)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
